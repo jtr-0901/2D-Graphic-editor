f1078d24b77b2b441f2ca2f6ba539d06e3692f18
jtr-0901 <jaganathtreddy2008@gmail.com>
2026-06-07 18:36:03 +0530

    Implement drawLine function for canvas
    Added drawLine function to draw lines on the canvas.

--- canvas.c
+++ canvas.c
@@ -27,3 +27,36 @@ void displayCanvas()
         printf("\n");
     }
 }
+
+#include <stdlib.h>
+
+void drawLine(int x1, int y1, int x2, int y2)
+{
+    int dx = x2 - x1;
+    int dy = y2 - y1;
+
+    int steps;
+
+    if(abs(dx) > abs(dy))
+        steps = abs(dx);
+    else
+        steps = abs(dy);
+
+    float xIncrement = dx / (float)steps;
+    float yIncrement = dy / (float)steps;
+
+    float x = x1;
+    float y = y1;
+
+    for(int i = 0; i <= steps; i++)
+    {
+        if((int)y >= 0 && (int)y < ROWS &&
+           (int)x >= 0 && (int)x < COLS)
+        {
+            canvas[(int)y][(int)x] = '*';
+        }
+
+        x += xIncrement;
+        y += yIncrement;
+    }
+}
