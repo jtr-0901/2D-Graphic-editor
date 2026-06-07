7e97e64d6bfb46ddc62f7f24ae00093fb528e2cc
jtr-0901 <jaganathtreddy2008@gmail.com>
2026-06-07 18:41:22 +0530

    Implement drawTriangle function
    Added a function to draw a triangle on the canvas.

--- canvas.c
+++ canvas.c
@@ -75,3 +75,10 @@ void drawRectangle(int x, int y, int width, int height)
     // Right edge
     drawLine(x + width, y, x + width, y + height);
 }
+
+void drawTriangle(int x1, int y1,int x2, int y2,int x3, int y3)
+{
+    drawLine(x1, y1, x2, y2);
+    drawLine(x2, y2, x3, y3);
+    drawLine(x3, y3, x1, y1);
+}
