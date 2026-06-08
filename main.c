b7305ce6b5c2cf4cdda1ecfd36c5789e042363eb
jtr-0901 <jaganathtreddy2008@gmail.com>
2026-06-08 08:55:28 +0530

    Update main function to draw shapes

--- main.c
+++ main.c
@@ -3,7 +3,9 @@
 int main()
 {
     initializeCanvas();
-    drawCircle(20, 10, 8);
+    drawRectangle(2,2,15,8);
+    drawTriangle(30,2,20,15,40,15);
+    drawCircle(60,10,7);
     displayCanvas();
     return 0;
 }
