3918dc0b107376a54645971197d7723c0f1aab43
jtr-0901 <jaganathtreddy2008@gmail.com>
2026-06-08 08:49:41 +0530

    Update canvas.h

--- canvas.h
+++ canvas.h
@@ -11,5 +11,6 @@ void displayCanvas();
 void drawLine(int x1, int y1, int x2, int y2);
 void drawRectangle(int x, int y, int width, int height);
 void drawTriangle(int x1, int y1,int x2, int y2,int x3, int y3);
+void drawCircle(int xc, int yc, int r);
 
 #endif
