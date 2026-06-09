0ae77bad0ac043949ad68e1ae0e83eb7a444af66
jtr-0901 <jaganathtreddy2008@gmail.com>
2026-06-09 18:10:34 +0530

    Implement addLine function for adding line shapes
    Added a function to add a line shape to the shapes array.

--- shapes.c
+++ shapes.c
@@ -2,3 +2,21 @@
 
 Shape shapes[MAX_SHAPES];
 int shapeCount = 0;
+
+void addLine(int x1, int y1, int x2, int y2)
+{
+    Shape s;
+
+    s.id = shapeCount + 1;
+    s.type = LINE;
+
+    s.x1 = x1;
+    s.y1 = y1;
+    s.x2 = x2;
+    s.y2 = y2;
+
+    s.active = 1;
+
+    shapes[shapeCount] = s;
+    shapeCount++;
+}
